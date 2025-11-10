f0b54e31ff01cb96aeaa4cf5eac95efaaf32b622
0-3AkhmadRafiHaidarrahman <nakanom858@gmail.com>
2025-11-10 14:12:59 +0700

    test

---
+++ index.js
@@ -0,0 +1,30 @@
+/*
+    Array => adalah sebuah struktur data yang bisa menyimpan lebih dari satu nilai
+    index = urutan data di dalam array
+    index start 0
+    ayam, ikan, kambing
+    
+    Syntax membuat array 
+    cara umumnya:
+    keywordVariabel NamaArray = [Nilainya]
+
+    ES6
+    KeywordVariabel NamaArray = new Array(Panjang Datanya)
+*/
+
+// Panjang data diambil dari jumlah nilainya 
+let buah = ["Apel","NAnas","Jeruk","Pisang"]
+console.log(buah.length)
+let dataNilai = new Array(36)
+console.log(dataNilai.length)
+console.log(typeof(dataNilai))
+
+
+// Function => block code yang dimana fungsinya untuk 
+// menampung sebuah logic
+
+// push() => untuk menambahkan item di index terakhir 
+// namaArray.push("ikan")
+console.log(`Data Array:${buah}`)
+buah.push("anggur")
+console.log(`Data Array sesudah di Push:${buah}`)
